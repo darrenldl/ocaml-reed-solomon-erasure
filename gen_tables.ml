fbf9fd99d66b5a02bb623f21c715c32382d9873d
Darren Ldl <darrenldldev@gmail.com>
2018-05-29 16:50:50 +1000

    Working on gen_tables.ml

--- gen_tables.ml
+++ gen_tables.ml
@@ -8,7 +8,7 @@ let gen_log_table (polynomial : int) : bytes =
   let result : bytes   = Bytes.make field_size '\x00' in
   let b      : int ref = ref 1 in
 
-  for log = 0 to field_size-2 do
+  for log = 0 to (field_size-1) - 1 do
     Bytes.set result !b (Char.chr log);
 
     b := !b lsl 1;
@@ -20,3 +20,60 @@ let gen_log_table (polynomial : int) : bytes =
 
   result
 
+let exp_table_size : int = field_size * 2 - 2
+
+let gen_exp_table (log_tabe : bytes) : bytes =
+  let result : bytes = Bytes.make exp_table_size '\x00' in
+
+  for i = 1 to (field_size) - 1 do
+    let log = Char.code (Bytes.get log_tabe i) in
+    Bytes.set result log                    (Char.chr i);
+    Bytes.set result (log + field_size - 1) (Char.chr i);
+  done;
+
+  result
+
+let multiply
+    (log_table : bytes)
+    (exp_table : bytes)
+    (a : int)
+    (b : int)
+  : int =
+  if a = 0 && b = 0 then
+    0
+  else (
+    let log_a = Char.code (Bytes.get log_table a) in
+    let log_b = Char.code (Bytes.get log_table b) in
+    let log_result = log_a + log_b in
+    Char.code (Bytes.get exp_table log_result)
+  )
+
+let gen_mul_table
+    (log_table : bytes)
+    (exp_table : bytes)
+  : bytes =
+
+  let result : bytes = Bytes.make (256 * 256) '\x00' in
+
+  for a = 0 to (field_size) - 1 do
+    for b = 0 to (field_size) - 1 do
+      Bytes.set result (a * 256 + b) (Char.chr (multiply log_table exp_table a b))
+    done
+  done;
+
+  result
+
+let gen_mul_table_half
+    (log_table : bytes)
+    (exp_table : bytes)
+  : bytes * bytes =
+  let half_table_length = 16 * field_size in
+
+  let low  : bytes = Bytes.make half_table_length '\x00' in
+  let high : bytes = Bytes.make half_table_length '\x00' in
+
+  for a = 0 to (half_table_length) - 1 do
+    for b = 0 to (half_table_length) - 1 do
+      let result : int ref = ref 0 in
+    done
+  done
