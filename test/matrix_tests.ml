cf7c65d458d79725fea348400d819e6584928969
Darren Ldl <darrenldldev@gmail.com>
2018-05-30 23:46:18 +1000

    Added matrix_tests

--- test/matrix_tests.ml
+++ test/matrix_tests.ml
@@ -3,22 +3,111 @@ open OUnit2
 
 let make_random_matrix (size : int) : matrix =
   let data = Array.make size Bytes.empty in
-  let data = Array.map (fun _ -> Bytes.map (fun _ -> char_of_int (Random.int 256)) (Bytes.make 100 '\x00')) data in
+  let data = Array.map (fun _ -> Bytes.map (fun _ -> char_of_int (Random.int 256)) (Bytes.make 100 '\000')) data in
   make_with_data data
 
+let test_matrix_col_count test_ctxt =
+  let m1 = make_with_data [|Bytes.of_string "\001\000\000"|] in
+  let m2 = make_with_data [|Bytes.of_string "\000\000\000";
+                            Bytes.of_string "\000\000\000"|] in
+  let m3 = make 1 4 in
+
+  assert_equal 3 (col_count m1);
+  assert_equal 3 (col_count m2);
+  assert_equal 4 (col_count m3)
+
 let test_matrix_row_count test_ctxt =
-  let m1 = make_with_data [|Bytes.of_string "\x01\x00\x00"|] in
-  let m2 = make_with_data [|Bytes.of_string "\x00\x00\x00";
-                            Bytes.of_string "\x00\x00\x00"|] in
+  let m1 = make_with_data [|Bytes.of_string "\001\000\000"|] in
+  let m2 = make_with_data [|Bytes.of_string "\000\000\000";
+                            Bytes.of_string "\000\000\000"|] in
   let m3 = make 1 4 in
 
   assert_equal 1 (row_count m1);
   assert_equal 2 (row_count m2);
   assert_equal 1 (row_count m3)
 
+let test_matrix_swap_rows test_ctxt =
+  begin
+    let m1     = make_with_data [|Bytes.of_string "\001\002\003";
+                                  Bytes.of_string "\004\005\006";
+                                  Bytes.of_string "\007\008\009"|] in
+    let expect = make_with_data [|Bytes.of_string "\007\008\009";
+                                  Bytes.of_string "\004\005\006";
+                                  Bytes.of_string "\001\002\003"|] in
+
+    swap_rows m1 0 2;
+    assert_equal expect m1;
+  end;
+  begin
+    let m1     = make_with_data [|Bytes.of_string "\001\002\003";
+                                  Bytes.of_string "\004\005\006";
+                                  Bytes.of_string "\007\008\009"|] in
+    let expect = copy m1 in
+    swap_rows m1 0 0;
+    assert_equal expect m1;
+    swap_rows m1 1 1;
+    assert_equal expect m1;
+    swap_rows m1 2 2;
+    assert_equal expect m1;
+  end
+
+let test_inconsistent_row_sizes test_ctxt =
+  try
+    make_with_data [|Bytes.of_string "\001\000\000";
+                     Bytes.of_string "\000\000";
+                     Bytes.of_string "\000\000\001"|] |> ignore;
+    assert_failure "Missing exception"
+  with
+  | Failure _ -> ()
+
+let test_incompatible_multiply test_ctxt =
+  try
+    let m1 = make_with_data [|Bytes.of_string "\000\001";
+                              Bytes.of_string "\000\001";
+                              Bytes.of_string "\000\001"|] in
+    let m2 = make_with_data [|Bytes.of_string "\000\001\002"|] in
+    multiply m1 m2 |> ignore;
+    assert_failure "Missing exception"
+  with
+  | Failure _ -> ()
+
+let test_incompatible_augment test_ctxt =
+  try
+    let m1 = make_with_data [|Bytes.of_string "\000\001"|] in
+    let m2 = make_with_data [|Bytes.of_string "\000\001";
+                              Bytes.of_string "\002\003"|] in
+    augment m1 m2 |> ignore;
+    assert_failure "Missing exception"
+  with
+  | Failure _ -> ()
+
+let test_matrix_identity test_ctxt =
+  let m1 = identity 3 in
+  let m2 = make_with_data [|Bytes.of_string "\001\000\000";
+                            Bytes.of_string "\000\001\000";
+                            Bytes.of_string "\000\000\001"|] in
+  assert_equal m1 m2
+
+let test_matrix_multiply test_ctxt =
+  let m1 = make_with_data [|Bytes.of_string "\001\002";
+                            Bytes.of_string "\003\004"|] in
+  let m2 = make_with_data [|Bytes.of_string "\005\006";
+                            Bytes.of_string "\007\008"|] in
+  let actual = multiply m1 m2 in
+  let expect = make_with_data [|Bytes.of_string "\011\022";
+                                Bytes.of_string "\019\042"|] in
+  assert_equal actual expect
+
 let suite =
   "matrix_tests">:::
-  ["test_matrix_row_count">:: test_matrix_row_count;
+  ["test_matrix_col_count">::       test_matrix_col_count;
+   "test_matrix_row_count">::       test_matrix_row_count;
+   "test_matrix_swap_rows">::       test_matrix_swap_rows;
+   "test_inconsistent_row_sizes">:: test_inconsistent_row_sizes;
+   "test_incompatible_multiply">::  test_incompatible_multiply;
+   "test_incompatible_augment">::   test_incompatible_augment;
+   "test_matrix_identity">::        test_matrix_identity;
+   "test_matrix_multiply">::        test_matrix_multiply;
   ]
 
 let () =
