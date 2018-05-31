444a84cf13c94f2810a6f2ea98f7c133997e2277
Darren Ldl <darrenldldev@gmail.com>
2018-05-31 19:37:36 +1000

    Added inversion_tree_tests to main test suite

--- test/inversion_tree_tests.ml
+++ test/inversion_tree_tests.ml
@@ -1,2 +1,17 @@
 open OUnit2
+open Reed_solomon_erasure__
 open Reed_solomon_erasure__.Inversion_tree
+
+let test_new_inversion_tree test_ctxt =
+  let tree = make 3 2 in
+
+  let expect = Matrix.make_with_data [|Bytes.of_string "\001\000\000";
+                                       Bytes.of_string "\000\001\000";
+                                       Bytes.of_string "\000\000\001"|] in
+
+  assert_equal (Some expect) (get_inverted_matrix tree [])
+
+let suite =
+  "inversion_tree_tests">:::
+  ["test_new_inversion_tree">:: test_new_inversion_tree;
+  ]
--- test/run_all_tests.ml
+++ test/run_all_tests.ml
@@ -2,4 +2,5 @@ open OUnit2
 
 let () =
   run_test_tt_main Matrix_tests.suite;
-  run_test_tt_main Galois_tests.suite
+  run_test_tt_main Galois_tests.suite;
+  run_test_tt_main Inversion_tree_tests.suite;
